bca4f94795ccecede194f13183a56ff08e9892e9
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-08 12:52:51 -0300

    feat: called 2 backend endpoints in user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private baseUrl = `${environment.apiUrl}/users`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllUsers() {
+    return this.http.get(this.baseUrl);
+  }
+
+  getUserById(id: number) {
+    return this.http.get(this.baseUrl + '/users/' + id);
+  }
 }
