18a41968c6a702a2fbd482f6303ccf7d2fb5b12a
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-08 13:46:57 -0300

    wip: getAllContacts endpoint, will proceed to making the table component

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  private baseUrl = `${environment.apiUrl}/contacts`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllContacts(){
+    return this.baseUrl;
+  }
+
+  getContactById(id: number) {
+    return this.http.get(this.baseUrl + id);
+  }
 }
