e830dfa010de259fcdd1f4f3f3772ecb9a554c1d
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-08 13:26:21 -0300

    wip: testing api call, first method works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from './services/user.service';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [NgForOf],
+  template: `<h1>Users</h1><ul><li *ngFor="let user of users">{{ user.name }}</li></ul>`
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'contact-manager-front';
+
+  users: any[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getAllUsers().subscribe({
+      next: (data: any) => this.users = data,
+      error: (err) => console.error('Error getting users', err),
+      }
+    )
+  }
 }
