7655070f8b909697ddc1f2138e614d0853e79d63
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-09 15:51:56 -0300

    wip: contacts table, will test now

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ContactService} from '../../services/contact.service';
+import {Contact} from '../../models/contact';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import {FilterPipe} from '../../pipes/filter.pipe';
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf,
+    FilterPipe
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
+  contacts: Contact[] = [];
+  searchTerm: string = "";
 
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.loadContacts();
+  }
+
+  loadContacts() {
+    this.contactService.getAll().subscribe((data) => {
+      this.contacts = data;
+    });
+  }
+
+  editContact(contact: Contact): void {
+    console.log('Editar contato', contact);
+  }
+
+  deleteContact(contact: Contact): void {
+    console.log('Apagar contato', contact);
+  }
 }
