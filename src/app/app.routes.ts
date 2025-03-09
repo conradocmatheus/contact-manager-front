277e1b6bac81ded0112d649116944997d3d8a666
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-09 00:29:10 -0300

    feat: added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'profile', component: UserProfileComponent },
+];
