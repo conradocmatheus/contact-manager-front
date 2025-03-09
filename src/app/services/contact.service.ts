afd5da692a3c62e171cb30ef0f55189742fc5e03
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-09 00:48:53 -0300

    feat: contact service rework

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Contact } from '../models/contact';
 import { environment } from '../../environments/environment.development';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Contact} from '../models/contact';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ContactService {
   private apiUrl = `${environment.apiUrl}/contacts`;
 
   constructor(private http: HttpClient) {}
 
-  getAllContacts(): Observable<Contact[]> {
+  getAll(): Observable<Contact[]> {
     return this.http.get<Contact[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Contact> {
+    return this.http.get<Contact>(`${this.apiUrl}/${id}`);
+  }
+
+  create(contact: Contact): Observable<Contact> {
+    return this.http.post<Contact>(this.apiUrl, contact);
+  }
+
+  update(id: number, contact: Contact): Observable<Contact> {
+    return this.http.put<Contact>(`${this.apiUrl}/${id}`, contact);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
