2377f3d3539748af6193c9fb7b50391c4e6afe29
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-09 15:38:06 -0300

    wip: sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
