f3cf8b84ef271ca1dff804ed3cfab749110c577d
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-09 00:35:04 -0300

    style: removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import {Component} from '@angular/core';
-import {ContactTableComponent} from './components/contacts-table/contacts-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [ContactTableComponent]
 })
 export class AppComponent {}
