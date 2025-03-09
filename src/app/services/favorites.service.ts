60c67bb5773898e9b8578b30a49cc953634a9ad0
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-09 00:48:09 -0300

    feat: favorite service rework

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FavoriteContact } from '../models/favoriteContact';
+import { environment } from '../../environments/environment.development';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class FavoritesService {
+@Injectable({ providedIn: 'root' })
+export class FavoriteContactService {
+  private apiUrl = `${environment.apiUrl}/favorites`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAll(userId: number): Observable<FavoriteContact[]> {
+    return this.http.get<FavoriteContact[]>(`${this.apiUrl}/${userId}`);
+  }
+
+  getById(id: number): Observable<FavoriteContact> {
+    return this.http.get<FavoriteContact>(`${this.apiUrl}/${id}`);
+  }
+
+  addFavorite(favorite: FavoriteContact): Observable<FavoriteContact> {
+    return this.http.post<FavoriteContact>(this.apiUrl, favorite);
+  }
+
+  removeFavorite(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
