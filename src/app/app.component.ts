292eef18dedb8602b10d12af7787e1e44cc06358
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-09 16:14:13 -0300

    chore: removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import {ContactsComponent} from './pages/contacts/contacts.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, ContactsComponent],
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
