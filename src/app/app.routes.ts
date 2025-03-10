772fbd3022a4e375987396779681a6add74f99e5
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-10 16:35:58 -0300

    chore: removed favorites component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
-import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { AboutComponent } from './pages/about/about.component';
 import { EmptyLayoutComponent } from './shared/empty-layout/empty-layout.component';
@@ -25,7 +24,6 @@ export const routes: Routes = [
     children: [
       { path: 'home', component: HomeComponent },
       { path: 'contacts', component: ContactsComponent },
-      { path: 'favorites', component: FavoritesComponent },
       { path: 'profile', component: UserProfileComponent },
       { path: 'about', component: AboutComponent },
     ],
