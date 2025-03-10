1bb0e12d7d470249b7f13afa6d08435acbc13ab8
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-09 21:53:46 -0300

    feat: about components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import {AboutComponent} from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,4 +13,5 @@ export const routes: Routes = [
   { path: 'contacts', component: ContactsComponent },
   { path: 'favorites', component: FavoritesComponent },
   { path: 'profile', component: UserProfileComponent },
+  { path: 'about', component: AboutComponent },
 ];
