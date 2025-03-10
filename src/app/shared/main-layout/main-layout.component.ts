2b4a9ee3a94f2661f5cda32493ba529a017aa61f
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-10 16:43:22 -0300

    chore: greetings for the user, calling its name

--- src/app/shared/main-layout/main-layout.component.ts
+++ src/app/shared/main-layout/main-layout.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SidebarComponent} from '../sidebar/sidebar.component';
 import {RouterOutlet} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -11,6 +12,15 @@ import {RouterOutlet} from '@angular/router';
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit {
+  userName: string | null = null;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const user = this.authService.getCurrentUser();
+    if (user) {
+      this.userName = user.name;
+    }
+  }
 }
