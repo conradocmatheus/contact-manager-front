0d6dfa8b6262195cc222ae93f1ddb0165d44560d
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-12 15:48:36 -0300

    feat: added http interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { NgToastModule } from 'ng-angular-popup';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { AuthInterceptorService } from './app/interceptors/auth-interceptor.service';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
-    importProvidersFrom(NgToastModule)
+    importProvidersFrom(NgToastModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
   ]
 }).catch((err) => console.error(err));
