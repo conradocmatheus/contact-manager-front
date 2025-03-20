45496570afcad7ed89c3937be62ffbf66e084765
conradocmatheus <conrado.matheus@protonmail.com>
2025-03-19 22:40:33 -0300

    fix: new interceptor for ngrok to function properly

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { NgToastModule } from 'ng-angular-popup';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AuthInterceptorService } from './app/interceptors/auth-interceptor.service';
+import { NgrokInterceptor } from './app/interceptors/ngrok.interceptor';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
@@ -17,6 +18,11 @@ bootstrapApplication(AppComponent, {
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: NgrokInterceptor,
       multi: true
     }
   ]
