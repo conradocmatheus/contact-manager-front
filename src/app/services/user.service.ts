02c485b7c4f8c44974de4610713b7e72e435a27b
Pedro Majewski <Pedrohmssilva@gmail.com>
2026-08-04 15:30:08 -0300

    Revert "Merge branch 'fix/remove-duplicate-unnecessary-endpoints' into dev"
    This reverts commit 064be720e6d7d08d4e9898dc7d17a5eead88c47d, reversing
    changes made to 5367e47d75607e8cf6c1181bee58731677115d64.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
@@ -10,15 +10,23 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  updateProfile(id: number, userData: { name: string, email: string }): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, userData);
+  create(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
   }
 
-  updatePassword(id: number, passwordData: { currentPassword: string, newPassword: string }): Observable<any> {
-    return this.http.put(`${environment.apiUrl}/auth/password/${id}`, passwordData);
+  getProfile(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/me`);
   }
 
-  delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  updateProfile(userData: { name: string, email: string }): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/me`, userData);
+  }
+
+  updatePassword(passwordData: { currentPassword: string, newPassword: string }): Observable<void> {
+    return this.http.put<void>(`${environment.apiUrl}/auth/password`, passwordData);
+  }
+
+  delete(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/me`);
   }
 }
